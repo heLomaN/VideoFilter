fd6443a524eaddb2f7d7f1e87de3f90da09b2d2e
heLomaN <caozhk@gmail.com>
2024-05-15 23:54:47 +0800

    a 0.1.0 version

---
+++ VideoFilter.bak.py
@@ -0,0 +1,148 @@
+import os, sys
+import ffmpeg
+from multiprocessing import Pool, cpu_count
+import cv2
+import numpy as np
+import tkinter as tk
+from tkinter import filedialog
+
+def create_folder(wk_path):
+    try:
+        os.mkdir(wk_path)
+    except Exception  as e:
+        print(str(e))
+
+
+def concat_vh(list_2d):
+    # return final image
+    return cv2.vconcat([cv2.hconcat(list_h)
+                        for list_h in list_2d])
+
+cache_path = "_video_filter"
+
+def generate_thubnail_idx(input_data):
+    (in_filename, time_offset) = input_data
+    try:
+        probe = ffmpeg.probe(in_filename)
+        time = float(probe['streams'][0]['duration']) // 5
+        width = probe['streams'][0]['width']
+    except Exception as e:
+        print(str(e) + " file=" + in_filename, file=sys.stderr)
+        return None
+
+    for idx in [1, 2, 3, 4]:
+        img_path = os.path.join(cache_path, "{}_{}.jpg".format(os.path.basename(in_filename), idx))
+        if os.path.exists(img_path):
+            os.remove(img_path)
+        try:
+            (
+                ffmpeg
+                .input(in_filename, ss=time*idx + time_offset)
+                .filter('scale', width, -1)
+                .output(img_path, vframes=1)
+                .overwrite_output()
+                .run(capture_stdout=True, capture_stderr=True)
+            )
+        except ffmpeg.Error as e:
+            print(e.stderr.decode(), file=sys.stderr)
+            # sys.exit(1)
+
+    return None
+
+def prepare_thumbnail(in_filename_list):
+    work_datas = [[in_filename, 0] for in_filename in in_filename_list]
+    p = Pool(cpu_count() - 1)
+    p.map(generate_thubnail_idx, work_datas)
+
+
+def generate_thumbnail(in_filename_list, delete_folder):
+    for idx, in_filename in enumerate(in_filename_list):
+        size_in_gbyte = os.path.getsize(in_filename) / 1024 / 1024 / 1024
+        text_label = "[ {}/{} ] {:.4f}GB press D to delete, X to stop, R to refresh, else to skip".format(idx+1, len(in_filename_list), size_in_gbyte)
+
+        offset = 0
+        while True:
+            image_file_path_list = [os.path.join(cache_path, "{}_{}.jpg".format(os.path.basename(in_filename), idx)) for idx in [1, 2, 3, 4]]
+            try:
+                img_list = [cv2.imdecode(np.fromfile(fp,dtype=np.uint8),-1) for fp in image_file_path_list]
+            except Exception as e:
+                print(str(e))
+                break
+
+            img_list_2d = [[img_list[0],img_list[1]],
+                           [img_list[2],img_list[3]]] if img_list[0].shape[0] < img_list[0].shape[1] else \
+                [[img_list[0], img_list[1], img_list[2], img_list[3]]]
+            img1 = concat_vh(img_list_2d)
+            cv2.putText(img1, text_label, org=(10,30), fontFace= cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.7, color=(255,128,128), thickness=1, lineType=cv2.LINE_AA)
+
+            window_name = os.path.basename(in_filename)
+            try:
+                window_name = window_name.encode("gbk").decode('UTF-8', errors='ignore')
+            except Exception as e:
+                print(str(e) + " skip " + in_filename)
+                break
+            # cv2.namedWindow(window_name)
+            cv2.namedWindow(window_name, cv2.WND_PROP_FULLSCREEN)
+            cv2.moveWindow(window_name, 200, 200)
+            cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+            src_size_h, src_size_w = (img1.shape[0], img1.shape[1]) # height & width
+            tgt_size_w, tgt_size_h = (2560, 1440) # width & height
+            img_bg = np.zeros([tgt_size_h, tgt_size_w, 3], dtype=np.uint8)
+            if src_size_h / src_size_w > tgt_size_h / tgt_size_w:
+                tgt_size_w = tgt_size_h * src_size_w / src_size_h
+            else:
+                tgt_size_h = tgt_size_w * src_size_h / src_size_w
+
+            tgt_size_tuple = (int(tgt_size_w), int(tgt_size_h))
+            cv2.resizeWindow(window_name, *tgt_size_tuple)
+
+            scaled_image = cv2.resize(img1, tgt_size_tuple)
+            cv2.imshow(window_name, scaled_image)
+
+            pressed = cv2.waitKey(0)
+            cv2.destroyAllWindows()
+            if chr(pressed & 255) in 'rR':
+                offset += 90
+                generate_thubnail_idx((in_filename, offset))
+            else:
+                if chr(pressed & 255) in 'dD':
+                    os.rename(in_filename, os.path.join(delete_folder, os.path.basename(in_filename)))
+                    print(in_filename, " delete")
+                elif chr(pressed & 255) in 'xX':
+                    exit(0)
+                break
+
+def ext_is_video(ext):
+    valid_ext = [".mp4", ".wmv", ".avi", ".mkv", ".avi", ".mpg", ".flv"]
+    return str.lower(ext) in valid_ext
+
+def glob_folders(top_path):
+    if not os.access(top_path, os.R_OK):
+        return
+    create_folder(cache_path)
+    delete_folder = os.path.join(top_path, cache_path)
+    create_folder(delete_folder)
+
+    files_to_check = []
+    for entry in os.scandir(top_path):
+        if os.access(entry, os.R_OK) and os.path.isfile(entry.path):
+            print(entry.path)
+            if ext_is_video(os.path.splitext(os.path.basename(entry.path))[1]):
+                files_to_check.append(entry.path)
+
+    # 多进程生成缩略图
+    prepare_thumbnail(files_to_check)
+    # 开始筛选是否保留视频
+    generate_thumbnail(files_to_check, delete_folder)
+
+def filt_video(top_path):
+    for entry in os.scandir(top_path):
+        if os.access(entry, os.R_OK) and os.path.isfile(entry.path):
+            if os.path.splitext(os.path.basename(entry.path))[1] == ".mp4":
+                pass
+
+if __name__ == "__main__":
+    root_dir = filedialog.askdirectory()
+    # root_dir = r"H:\photos\photos_current\20220405运动相机\100XTUDV"
+    glob_folders(root_dir)
--- VideoFilter.py
+++ VideoFilter.py
@@ -1,148 +1,228 @@
-import os, sys
-import ffmpeg
-from multiprocessing import Pool, cpu_count
+import sys
+import os
 import cv2
-import numpy as np
-import tkinter as tk
-from tkinter import filedialog
+import sqlite3
+import configparser
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QFileDialog, QLabel, QListWidget, \
+    QListWidgetItem, QHBoxLayout
+from PyQt5.QtGui import QPixmap, QImage, QKeyEvent, QColor, QDesktopServices
+from PyQt5.QtCore import Qt, QUrl
 
-def create_folder(wk_path):
-    try:
-        os.mkdir(wk_path)
-    except Exception  as e:
-        print(str(e))
+class RecentDirectoryHandler:
+    def __init__(self):
+        self.config_file = 'config.ini'
 
+    def get_last_used_directory(self):
+        config = configparser.ConfigParser()
+        config.read(self.config_file)
 
-def concat_vh(list_2d):
-    # return final image
-    return cv2.vconcat([cv2.hconcat(list_h)
-                        for list_h in list_2d])
-
-cache_path = "_video_filter"
-
-def generate_thubnail_idx(input_data):
-    (in_filename, time_offset) = input_data
-    try:
-        probe = ffmpeg.probe(in_filename)
-        time = float(probe['streams'][0]['duration']) // 5
-        width = probe['streams'][0]['width']
-    except Exception as e:
-        print(str(e) + " file=" + in_filename, file=sys.stderr)
-        return None
-
-    for idx in [1, 2, 3, 4]:
-        img_path = os.path.join(cache_path, "{}_{}.jpg".format(os.path.basename(in_filename), idx))
-        if os.path.exists(img_path):
-            os.remove(img_path)
         try:
-            (
-                ffmpeg
-                .input(in_filename, ss=time*idx + time_offset)
-                .filter('scale', width, -1)
-                .output(img_path, vframes=1)
-                .overwrite_output()
-                .run(capture_stdout=True, capture_stderr=True)
+            return config['DEFAULT']['LastDirectory']
+        except KeyError:
+            return os.getcwd()  # 默认使用当前工作目录
+
+    def save_last_used_directory(self, directory):
+        config = configparser.ConfigParser()
+        config['DEFAULT'] = {'LastDirectory': directory}
+        with open(self.config_file, 'w') as configfile:
+            config.write(configfile)
+class VideoThumbnailer(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        self.last_directory = RecentDirectoryHandler()
+        self.db_connection = None
+        self.to_delete = []
+
+    def initUI(self):
+        self.layout = QVBoxLayout()
+
+        self.btnLayout = QHBoxLayout()
+        self.previewLayout = QHBoxLayout()
+
+        self.btnOpen = QPushButton('Open Directory', self)
+        self.btnOpen.clicked.connect(self.openDirectory)
+        self.btnLayout.addWidget(self.btnOpen)
+
+        self.btnPreview = QPushButton('Preview', self)
+        self.btnPreview.setEnabled(False)
+        self.btnPreview.clicked.connect(self.previewNext)
+        self.btnLayout.addWidget(self.btnPreview)
+
+        self.btnDisplayImage = QPushButton('Display Image', self)
+        self.btnDisplayImage.setEnabled(False)
+        self.btnDisplayImage.clicked.connect(self.displayImage)
+        self.btnLayout.addWidget(self.btnDisplayImage)
+
+        self.btnPlay = QPushButton('Play Video', self)
+        self.btnPlay.setEnabled(False)
+        self.btnPlay.clicked.connect(self.playItem)
+        self.btnLayout.addWidget(self.btnPlay)
+
+        self.btnDelete = QPushButton('Delete Selected', self)
+        self.btnDelete.clicked.connect(self.deleteSelected)
+        self.btnLayout.addWidget(self.btnDelete)
+        self.layout.addLayout(self.btnLayout)
+
+        self.listWidget = QListWidget(self)
+        self.listWidget.itemClicked.connect(self.listItemClicked)
+        self.listWidget.setMaximumWidth(400)
+        self.listWidget.setMinimumWidth(200)
+        self.previewLayout.addWidget(self.listWidget)
+
+        self.imageLabel = QLabel(self)
+        self.imageLabel.setText("Mouse right click to switch between delete and Keep, else to skip")
+        self.imageLabel.mouseReleaseEvent = self.imageClicked
+        self.imageLabel.setMinimumSize(600, 300)
+        self.previewLayout.addWidget(self.imageLabel)
+        self.layout.addLayout(self.previewLayout)
+
+        self.setLayout(self.layout)
+        self.setGeometry(300, 300, 1024, 768)
+        self.setWindowTitle('Video Thumbnailer')
+        self.show()
+
+    def openDirectory(self):
+        dir_path = QFileDialog.getExistingDirectory(self, "Select Directory", self.last_directory.get_last_used_directory())
+        if dir_path:
+            self.last_directory.save_last_used_directory(dir_path)
+            self.processDirectory(dir_path)
+
+    def processDirectory(self, root):
+        preview_dir = os.path.join(root, '.preview')
+        os.makedirs(preview_dir, exist_ok=True)
+        db_path = os.path.join(preview_dir, 'thumbnails.sqldb')
+        previewCreated = os.path.exists(db_path)
+        self.setupDatabase(db_path)
+        if not previewCreated:
+            for subdir, dirs, files in os.walk(root):
+                for file in files:
+                    filepath = os.path.join(subdir, file)
+                    if '.del' not in filepath and filepath.lower().endswith((".mp4", ".wmv", ".avi", ".mkv", ".avi", ".mpg", ".flv")):
+                        self.generateThumbnail(filepath, preview_dir)
+
+        self.loadVideoList()
+
+    def setupDatabase(self, db_path):
+        self.db_connection = sqlite3.connect(db_path)
+        cursor = self.db_connection.cursor()
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS Thumbnails (
+                id INTEGER PRIMARY KEY,
+                video_path TEXT,
+                thumbnail_path TEXT
             )
-        except ffmpeg.Error as e:
-            print(e.stderr.decode(), file=sys.stderr)
-            # sys.exit(1)
+        ''')
+        self.db_connection.commit()
 
-    return None
+    def generateThumbnail(self, video_path, preview_dir):
+        cap = cv2.VideoCapture(video_path)
+        length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        frames_to_capture = [length // 6 * i for i in range(1, 7)]
+        images = []
 
-def prepare_thumbnail(in_filename_list):
-    work_datas = [[in_filename, 0] for in_filename in in_filename_list]
-    p = Pool(cpu_count())
-    p.map(generate_thubnail_idx, work_datas)
+        for frame_number in frames_to_capture:
+            cap.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
+            ret, frame = cap.read()
+            if ret:
+                images.append(frame)
 
+        cap.release()
 
-def generate_thumbnail(in_filename_list, delete_folder):
-    for idx, in_filename in enumerate(in_filename_list):
-        size_in_gbyte = os.path.getsize(in_filename) / 1024 / 1024 / 1024
-        text_label = "[ {}/{} ] {:.4f}GB press D to delete, X to stop, R to refresh, else to skip".format(idx+1, len(in_filename_list), size_in_gbyte)
+        thumbnail_path = os.path.join(preview_dir, os.path.basename(video_path) + '.jpg')
+        self.saveConcatenatedImages(images, thumbnail_path)
+        self.saveThumbnailRecord(video_path, thumbnail_path)
 
-        offset = 0
-        while True:
-            image_file_path_list = [os.path.join(cache_path, "{}_{}.jpg".format(os.path.basename(in_filename), idx)) for idx in [1, 2, 3, 4]]
-            try:
-                img_list = [cv2.imdecode(np.fromfile(fp,dtype=np.uint8),-1) for fp in image_file_path_list]
-            except Exception as e:
-                print(str(e))
-                break
+    def saveConcatenatedImages(self, images, thumbnail_path):
+        if images:
+            while len(images) < 6:
+                images.append(images[-1])
+        concatenated_image = cv2.vconcat([cv2.hconcat(images[0:3]), cv2.hconcat(images[3:6])])
+        # do not support utf8 path
+        # cv2.imwrite(thumbnail_path, concatenated_image)
+        is_success, im_buf_arr = cv2.imencode(".jpg", concatenated_image)
+        im_buf_arr.tofile(thumbnail_path)
 
-            img_list_2d = [[img_list[0],img_list[1]],
-                           [img_list[2],img_list[3]]] if img_list[0].shape[0] < img_list[0].shape[1] else \
-                [[img_list[0], img_list[1], img_list[2], img_list[3]]]
-            img1 = concat_vh(img_list_2d)
-            cv2.putText(img1, text_label, org=(10,30), fontFace= cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.7, color=(255,128,128), thickness=1, lineType=cv2.LINE_AA)
+    def saveThumbnailRecord(self, video_path, thumbnail_path):
+        cursor = self.db_connection.cursor()
+        cursor.execute('INSERT INTO Thumbnails (video_path, thumbnail_path) VALUES (?, ?)', (video_path, thumbnail_path))
+        self.db_connection.commit()
 
-            window_name = os.path.basename(in_filename)
-            try:
-                window_name = window_name.encode("gbk").decode('UTF-8', errors='ignore')
-            except Exception as e:
-                print(str(e) + " skip " + in_filename)
-                break
-            # cv2.namedWindow(window_name)
-            cv2.namedWindow(window_name, cv2.WND_PROP_FULLSCREEN)
-            cv2.moveWindow(window_name, 200, 200)
-            cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+    def loadVideoList(self):
+        cursor = self.db_connection.cursor()
+        cursor.execute('SELECT video_path FROM Thumbnails')
+        videos = cursor.fetchall()
+        self.listWidget.clear()
+        for video in videos:
+            self.listWidget.addItem(video[0])
 
-            src_size_h, src_size_w = (img1.shape[0], img1.shape[1]) # height & width
-            tgt_size_w, tgt_size_h = (2560, 1440) # width & height
-            img_bg = np.zeros([tgt_size_h, tgt_size_w, 3], dtype=np.uint8)
-            if src_size_h / src_size_w > tgt_size_h / tgt_size_w:
-                tgt_size_w = tgt_size_h * src_size_w / src_size_h
+        self.btnDisplayImage.setEnabled(True)
+        self.btnPreview.setEnabled(True)
+        self.btnPlay.setEnabled(True)
+
+    def listItemClicked(self):
+        thumbnail_path = self.getCurrentItemThumbnailPath()
+        self.updateThumbnail(thumbnail_path)
+
+    def getCurrentItemThumbnailPath(self):
+        video_path = self.listWidget.currentItem().text()
+        thumbnail_path = self.getThumbnailpathFromVideoPath(video_path)
+        return thumbnail_path
+
+    def getThumbnailpathFromVideoPath(self, video_path):
+        cursor = self.db_connection.cursor()
+        cursor.execute('SELECT thumbnail_path FROM Thumbnails WHERE video_path=?', (video_path,))
+        return cursor.fetchone()[0]
+    def displayImage(self):
+        QDesktopServices.openUrl(QUrl.fromLocalFile(self.getCurrentItemThumbnailPath()))
+
+    def playItem(self):
+        video_path = self.listWidget.currentItem().text()
+        QDesktopServices.openUrl(QUrl.fromLocalFile(video_path))
+
+    def imageClicked(self, event):
+        if not self.listWidget.currentItem():
+            return None
+        video_path = self.listWidget.currentItem().text()
+        if event.button() == Qt.RightButton:
+            if video_path not in self.to_delete:
+                self.to_delete.append(video_path)
+                self.listWidget.currentItem().setBackground(Qt.red)
             else:
-                tgt_size_h = tgt_size_w * src_size_h / src_size_w
+                self.to_delete.remove(video_path)
+                self.listWidget.currentItem().setBackground(Qt.white)
+        self.previewNext()
 
-            tgt_size_tuple = (int(tgt_size_w), int(tgt_size_h))
-            cv2.resizeWindow(window_name, *tgt_size_tuple)
+    def updateThumbnail(self, thumbnail_path):
+        pixmap = QPixmap(thumbnail_path)
+        scaled_pixmap = pixmap.scaled(self.imageLabel.size(), Qt.KeepAspectRatio, Qt.SmoothTransformation)
+        self.imageLabel.setPixmap(scaled_pixmap)
+    def previewNext(self):
+        current_row = self.listWidget.currentRow()
+        next_row = current_row + 1 if current_row + 1 < self.listWidget.count() else 0
+        self.listWidget.setCurrentRow(next_row)
+        self.listItemClicked()
 
-            scaled_image = cv2.resize(img1, tgt_size_tuple)
-            cv2.imshow(window_name, scaled_image)
+    def deleteSelected(self):
+        del_dir = os.path.join(self.last_directory.get_last_used_directory(), '.del')
+        os.makedirs(del_dir, exist_ok=True)
 
-            pressed = cv2.waitKey(0)
-            cv2.destroyAllWindows()
-            if chr(pressed & 255) in 'rR':
-                offset += 90
-                generate_thubnail_idx((in_filename, offset))
-            else:
-                if chr(pressed & 255) in 'dD':
-                    os.rename(in_filename, os.path.join(delete_folder, os.path.basename(in_filename)))
-                    print(in_filename, " delete")
-                elif chr(pressed & 255) in 'xX':
-                    exit(0)
-                break
+        # 获取与待删除视频路径对应的数据库记录
+        conn = self.db_connection
+        cursor = conn.cursor()
 
-def ext_is_video(ext):
-    valid_ext = [".mp4", ".wmv", ".avi", ".mkv", ".avi", ".mpg", ".flv"]
-    return str.lower(ext) in valid_ext
+        for video_path in self.to_delete:
+            # 移动缩略图到.del文件夹
+            thumbnail_path = self.getThumbnailpathFromVideoPath(video_path)
+            os.rename(thumbnail_path, os.path.join(del_dir, os.path.basename(thumbnail_path)))
+            os.rename(video_path, os.path.join(del_dir, os.path.basename(video_path)))
+            # 从数据库中删除记录
+            cursor.execute('DELETE FROM Thumbnails WHERE video_path=?', (video_path,))
+            conn.commit()
 
-def glob_folders(top_path):
-    if not os.access(top_path, os.R_OK):
-        return
-    create_folder(cache_path)
-    delete_folder = os.path.join(top_path, cache_path)
-    create_folder(delete_folder)
-
-    files_to_check = []
-    for entry in os.scandir(top_path):
-        if os.access(entry, os.R_OK) and os.path.isfile(entry.path):
-            print(entry.path)
-            if ext_is_video(os.path.splitext(os.path.basename(entry.path))[1]):
-                files_to_check.append(entry.path)
-
-    # 多进程生成缩略图
-    prepare_thumbnail(files_to_check)
-    # 开始筛选是否保留视频
-    generate_thumbnail(files_to_check, delete_folder)
-
-def filt_video(top_path):
-    for entry in os.scandir(top_path):
-        if os.access(entry, os.R_OK) and os.path.isfile(entry.path):
-            if os.path.splitext(os.path.basename(entry.path))[1] == ".mp4":
-                pass
-
-if __name__ == "__main__":
-    root_dir = filedialog.askdirectory()
-    # root_dir = r"H:\photos\photos_current\20220405运动相机\100XTUDV"
-    glob_folders(root_dir)
+        self.to_delete.clear()
+        self.loadVideoList()
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = VideoThumbnailer()
+    sys.exit(app.exec_())
